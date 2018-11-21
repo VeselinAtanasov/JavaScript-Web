34c10e22d15b7db2183b6e2107539f9d8d6037bd
veselin.atanasov@opencode.com <veselin.atanasov@opencode.com>
2018-11-21 10:51:04 +0200

    BugFix: allow the app to work with empty database

--- ExpressJS/EXAM_FINAL/server/controllers/home-controller.js
+++ ExpressJS/EXAM_FINAL/server/controllers/home-controller.js
@@ -2,30 +2,33 @@ const User = require('../data/User');
 const api = require('../api/article-api.js');
 
 module.exports = {
-    index: async (req, res) => {
+  index: async (req, res) => {
+    let allArticles = await api.getAllArticles();
+    let latestArticle = allArticles[0];
 
-        let allArticles = await api.getAllArticles();
-        let latestArticle = allArticles[0];
-        if (allArticles.length > 3) {
-            allArticles.shift();
-        }
-
-        let lastEditId = latestArticle.edits[latestArticle.edits.length-1];
-        let getLatestArticalContent = await api.getLastEdit(lastEditId);
-        let lastContent = getLatestArticalContent.content.split(' ').slice(0,50).join(' ');
-        let data = {
-            allArticles: allArticles,
-            latestArticle: latestArticle,
-            lastContent :lastContent,
-            id : latestArticle._id
-        };
-
-        if (req.user) {
-            data.currentUser = {
-                name: req.user.username
-            };
-
-        }
-        res.render('home/index', data);
+    // In case DB is empty:
+    if (allArticles.length === 0) {
+      return res.render('home/index');
     }
+    if (allArticles.length > 3) {
+      allArticles.shift();
+    }
+
+    let lastEditId = latestArticle.edits[latestArticle.edits.length - 1];
+    let getLatestArticalContent = await api.getLastEdit(lastEditId);
+    let lastContent = getLatestArticalContent.content.split(' ').slice(0, 50).join(' ');
+    let data = {
+      allArticles: allArticles,
+      latestArticle: latestArticle,
+      lastContent: lastContent,
+      id: latestArticle._id
+    };
+
+    if (req.user) {
+      data.currentUser = {
+        name: req.user.username
+      };
+    }
+    res.render('home/index', data);
+  }
 };
